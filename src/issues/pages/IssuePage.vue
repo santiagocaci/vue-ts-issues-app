59df09925b560a97e34e3dd80779532bb9aa59d5
Santiago Caci <santiago.caci@gmail.com>
2022-12-14 17:47:05 -0300

    Initial project structure

---
+++ src/issues/pages/IssuePage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <span class="text-h2">Issue: 1</span>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/issues/pages/ListPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <span class="text-h2">List Issues Page</span>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
